89e812a8a6f86351ce06cd05e650d93b5d2d1033
Murphy316 <liqi316@live.com>
2020-06-02 21:05:48 -0700

    header and summary..

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import FetchRandomUser from "./components/FetchRandomUser";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          already edit, is that works fine?
-        </p>
-		<p>
-		  no more command line when you make changes on Github and you wants update website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    visible: true
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <FetchRandomUser />
+      </div>
+    );
+  }
 }
 
 export default App;
