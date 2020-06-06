c0c6e8e0255aa9b282e31a5ce0ae523973c93731
Murphy316 <liqi316@live.com>
2020-06-06 13:47:54 -0700

    loadmore v2.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-24 00:16:20
+ * @LastEditTime: 2020-06-06 13:38:26
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \OPReactViaGit\src\App.js
+ */ 
 import React, { Component } from "react";
 import "./App.css";
 import FetchLatestNews from "./components/FetchLatestNews";
+
 
 class App extends Component {
   state = {
--- src/components/FetchLatestNews.js
+++ src/components/FetchLatestNews.js
@@ -1,7 +1,7 @@
 /*
  * @Author: mm
  * @Date: 2020-06-02 10:39:02
- * @LastEditTime: 2020-06-05 09:43:00
+ * @LastEditTime: 2020-06-06 13:46:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \OPReactViaGit\src\components\FetchRandomUser.js
@@ -12,16 +12,18 @@
 
  import React, { useEffect, useState } from "react";
  import axios from "axios";
- 
+
  export default function FetchLatestNews() {
   const [data, setData] = useState([]);
-  const [query, setQuery] = useState();
-  const [count, setCount] = useState();
+  //const [query, setQuery] = useState();
+  const [skip, setSkip] = useState(0);
 
+  //default with nothing..
   const [url, setUrl] = useState(
-    'https://news.api.gov.bc.ca/api/Posts/Latest/home/default?postKind=releases&count=20&skip=0&api-version=1.0',
+    'https://news.api.gov.bc.ca/api/Posts/Latest/home/default?postKind=releases&count=5&skip=0&api-version=1.0',
   );
  
+  //??G
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(url);
@@ -33,35 +35,45 @@
  
   return (
     <React.Fragment>
-      <input
+      {/*<input
         type="text"
         value={query}
         onChange={event => setQuery(event.target.value)}
       />
-      <input
-        type="text"
-        value={count}
-        onChange={event => setCount(event.target.value)}
-      />
       <button
         type="button"
         onClick={() =>
-          setUrl(`https://news.api.gov.bc.ca/api/Posts/Latest/home/default?postKind=${query}&count=${count}&skip=0&api-version=1.0`)
+          setUrl(`https://news.api.gov.bc.ca/api/Posts/Latest/home/default?postKind=releases&count=3&skip=0&api-version=1.0`)
         }
       >
         Search
       </button>
- 
       <p>Search category: releases, stories, factsheets, updates or default</p>
+      */}
+
       <ul>
         {data.map(item => (
           <li key={item.atomId}>
             {item.documents.map(documents => <h4 key = {documents.languageId}>{documents.headline} </h4>)}
-            <b> news type:</b>  {item.kind} <br/><br/>
+            {/*<b> news type:</b>  {item.kind} <br/><br/>
             <b> Summary:</b>  {item.summary} 
+            */}
           </li> 
         ))}
       </ul>
+
+      <button
+          type="button"
+          onClick={() => {
+            setSkip(skip + 1);
+            setUrl(`https://news.api.gov.bc.ca/api/Posts/Latest/home/default?postKind=releases&count=5&skip=${skip}&api-version=1.0`);
+            
+          }
+          }
+        >
+          Load More
+        </button>
+
     </React.Fragment>
   );
 }
