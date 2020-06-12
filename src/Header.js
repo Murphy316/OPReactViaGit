5b21dc0bc963b8668d1191c3e0d52b198f002e39
Murphy316 <liqi316@live.com>
2020-06-11 20:42:39 -0700

    change href url to openshift site

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ function Header() {
       // Contents of the header
       <div id="header">
           <div id="header-top">
-            <a href="http://localhost:3000/">
+            <a href="http://react-web-app-via-git-xordpe-test.pathfinder.gov.bc.ca/">
               <img id="bc-gov-logo" src={require("./includes/gov_bc_logo.svg")} alt="bc-gov-logo" title="B.C. News Site" />
             </a>
           </div>
